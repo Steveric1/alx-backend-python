91e5f6db9678cca9b6a23cbb3262250d3a0fc746
Eric <stephen.erich243@gmail.com>
2024-05-07 20:04:32 +0100

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,18 +2,17 @@
 
 """Measure the runtime of async_comprehension four times in parallel using"""
 
-import time
-from typing import List
 import asyncio
+import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension four times in 
     parallel using asyncio.gather"""
 
     start_time = time.perf_counter()
-    list_comprehension = [async_comprehension() for _ in range(4)]
-    await asyncio.gather(*list_comprehension)
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     end_time = time.perf_counter()
     return end_time - start_time
