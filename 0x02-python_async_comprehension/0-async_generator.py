adbb644cd53554b33c84bca8172a70c1d4214be3
Eric <stephen.erich243@gmail.com>
2024-05-07 19:38:38 +0100

    task 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,11 @@ that will yield a random number between 0 and 10 every 1 second."""
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number every 1 second."""
-    for _ in range(10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 
 """Coroutine that takes no arguments and returns a generator object"""
 
-import asyncio
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing over async_generator"""
     list_comprehension = [i async for i in async_generator()]
     return list_comprehension
